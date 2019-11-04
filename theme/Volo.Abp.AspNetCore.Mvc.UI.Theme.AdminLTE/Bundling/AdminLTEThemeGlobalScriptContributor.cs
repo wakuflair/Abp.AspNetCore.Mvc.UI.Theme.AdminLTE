6874a16b64a3ce611190b0c0fbf08c05b7bcf993
郭洪鸣 <Administrator@42YOGCX83F15ST0>
2019-11-04 17:54:03 +0800

    将AdminLTE从2.4.18升级为3.0

--- theme/Volo.Abp.AspNetCore.Mvc.UI.Theme.AdminLTE/Bundling/AdminLTEThemeGlobalScriptContributor.cs
+++ theme/Volo.Abp.AspNetCore.Mvc.UI.Theme.AdminLTE/Bundling/AdminLTEThemeGlobalScriptContributor.cs
@@ -9,13 +9,14 @@ namespace Volo.Abp.AspNetCore.Mvc.UI.Theme.AdminLTE.Bundling
     {
         public override void ConfigureBundle(BundleConfigurationContext context)
         {
-            //context.Files.Clear();
-            // || t.Contains("/libs/datatables.net")
-            context.Files.RemoveAll(t => t.Contains("/libs/jquery/") || t.Contains("/libs/bootstrap/"));
+            ////context.Files.Clear();
+            //// || t.Contains("/libs/datatables.net")
+            //context.Files.RemoveAll(t => t.Contains("/libs/jquery/") || t.Contains("/libs/bootstrap/"));
 
-            context.Files.Insert(1, "/libs/jquery/jquery.min.js");
+            //context.Files.Insert(1, "/plugins/jquery/jquery.min.js");
 
-            context.Files.Insert(3, "/libs/bootstrap/js/bootstrap.js");
+            //context.Files.Insert(3, "/plugins/bootstrap/js/bootstrap.js");
+            context.Files.Add("/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js");
             context.Files.Add("/themes/adminlte/js/adminlte.js");
             context.Files.Add("/themes/adminlte/js/layout.js");
         }
